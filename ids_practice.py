70e36933c771dd50ac83e20b8d50b441999cdc97
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-03-25 04:10:49 -0700

    Progress to ids

--- ids_practice.py
+++ ids_practice.py
@@ -1,82 +1,78 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.cluster import KMeans
+from time import time
+from numpy import array
 
-# Change the following variables as desired.
-# 'k_value' is any positive integer
-k_value = 5
+names = ["duration","protocol_type","service","flag","src_bytes",
+    "dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins",
+    "logged_in","num_compromised","root_shell","su_attempted","num_root",
+    "num_file_creations","num_shells","num_access_files","num_outbound_cmds",
+    "is_host_login","is_guest_login","count","srv_count","serror_rate",
+    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
+    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
+    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
+    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
+    "dst_host_rerror_rate","dst_host_srv_rerror_rate","label"]
 
-# Assign column names to the dataset
-names = ['duration', 'protocol_type', 'service',\
-    'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment',\
-    'urgent', 'hot', 'num_failed_logins', 'logged_in', 'num_compromised',\
-    'root_shell', 'su_attempted', 'num_root', 'num_file_creations', 'num_shells',\
-    'num_access_files', 'num_outbound_cmds', 'is_host_login', 'is_guest_login', 'count',\
-    'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',\
-    'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate', 'dst_host_count',\
-    'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',\
-    'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',\
-    'dst_host_srv_serror_rate', 'dst_host_rerror_rate',' dst_host_srv_rerror_rate', 'class']
 
-# Read dataset to pandas dataframe
-train_dataset = pd.read_csv("KDDTrain+20Percent.txt", names=names)
+kdd_data_10percent = pd.read_csv("KDD10.txt", header=None, names=names)
+# print(kdd_data_10percent.describe())
 
-# Label encoder transforms the dataframe into integers accordinglyp
-label_encoder = preprocessing.LabelEncoder()
-transform_train = train_dataset.iloc[:, :-2].apply(label_encoder.fit_transform)
+# print(kdd_data_10percent['label'].value_counts())
+num_features = [
+    "duration","src_bytes",
+    "dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins",
+    "logged_in","num_compromised","root_shell","su_attempted","num_root",
+    "num_file_creations","num_shells","num_access_files","num_outbound_cmds",
+    "is_host_login","is_guest_login","count","srv_count","serror_rate",
+    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
+    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
+    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
+    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
+    "dst_host_rerror_rate","dst_host_srv_rerror_rate"
+]
+features = kdd_data_10percent[num_features].astype(float)
+# print(features.describe())
 
-km = KMeans(
-    n_clusters=3, init='random',
-    n_init=10, max_iter=300,
-    tol=1e-04, random_state=0
-)
-
-X_train = km.fit(transform_train.iloc[:, :-2].values)
-print(X_train)
-y_train_fit = train_dataset.iloc[:, 41].values
-
-test_dataset = pd.read_csv("KDDTest.txt", names=names)
-
-transform_test = test_dataset.iloc[:, :-2].apply(label_encoder.fit_transform)
-
-X_test_fit = transform_test.iloc[:, :-2].values
-y_test_fit = test_dataset.iloc[:, 41].values
-"""
-# Normalize the data for more accurate results
-scaler = preprocessing.StandardScaler()
-scaler.fit(X_train_fit)
-X_train = scaler.transform(X_train_fit)
-X_test = scaler.transform(X_test_fit)
-"""
-# Apply the k-nearest neighbors classifier
 from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors=k_value)
-classifier.fit(X_train, y_train_fit)
+labels = kdd_data_10percent['label'].copy()
+labels[labels!='normal.'] = 'attack.'
+# print(labels.value_counts())
 
-y_pred = classifier.predict(X_test)
-print(y_pred)   
+from sklearn.preprocessing import MinMaxScaler
+minmax = MinMaxScaler()
+features = pd.DataFrame(minmax.fit_transform(features.values), columns=num_features)
+# print(features)
 
-# Print out the results
-from sklearn.metrics import classification_report, confusion_matrix
-print(confusion_matrix(y_test_fit.astype(str), y_pred.astype(str)))
-print(classification_report(y_test_fit, y_pred))
+clf = KNeighborsClassifier(n_neighbors = 5, algorithm = 'ball_tree', leaf_size=500)
+t0 = time()
+clf.fit(features,labels)
+tt = time()-t0
+print("Classifier trained in {} seconds".format(round(tt,3)))
 
-# Calculating error for K values between 1 and 40
-error = []
-for i in range(1, 40):
-    knn = KNeighborsClassifier(n_neighbors=i)
-    knn.fit(X_train, y_train_fit)
-    pred_i = knn.predict(X_test)
-    error.append(np.mean(pred_i != y_test_fit))
-    print(error[i-1])
+kdd_data_corrected = pd.read_csv("corrected.txt", header=None, names = names)
+print(kdd_data_corrected['label'].value_counts())
 
-# Plot the Error Rate per k value graph
-plt.figure(figsize=(12, 6))
-plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o',
-         markerfacecolor='blue', markersize=10)
-plt.title('Error Rate K Value')
-plt.xlabel('K Value')
-plt.ylabel('Mean Error')
-plt.show()
+kdd_data_corrected['label'][kdd_data_corrected['label']!='normal.'] = 'attack.'
+print(kdd_data_corrected['label'].value_counts())
+
+from sklearn.model_selection import train_test_split
+kdd_data_corrected[num_features] = kdd_data_corrected[num_features].astype(float)
+kdd_data_corrected = pd.DataFrame(minmax.fit_transform(kdd_data_corrected[num_features].values), columns=num_features)
+# kdd_data_corrected[num_features].apply(lambda x: MinMaxScaler().fit_transform(x))
+
+features_train, features_test, labels_train, labels_test = train_test_split(
+    kdd_data_corrected[num_features], 
+    kdd_data_corrected['label'], 
+    test_size=0.1, 
+    random_state=42
+    )
+
+t0 = time()
+pred = clf.predict(features_test)
+tt = time() - t0
+print("Predicted in {} seconds".format(round(tt,3)))
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print(acc)
+print("R squared is {}.".format(round(acc,4)))
