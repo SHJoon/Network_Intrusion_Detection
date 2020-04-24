2a5e396a3bee495882e199c83e45890df657ac83
SHJoon <hojoon0617@gmail.com>
2020-04-23 23:36:40 -0700

    Furthur improvement to the code

--- ids_practice.py
+++ ids_practice.py
@@ -1,49 +1,24 @@
 import pandas as pd
 from time import time
 from numpy import array
-
-names = ["duration","protocol_type","service","flag","src_bytes",
-    "dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins",
-    "logged_in","num_compromised","root_shell","su_attempted","num_root",
-    "num_file_creations","num_shells","num_access_files","num_outbound_cmds",
-    "is_host_login","is_guest_login","count","srv_count","serror_rate",
-    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
-    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
-    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
-    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
-    "dst_host_rerror_rate","dst_host_srv_rerror_rate","label"]
-
+from utils.codes import *
 
 kdd_data_10percent = pd.read_csv("KDD10.txt", header=None, names=names)
-print(kdd_data_10percent.describe())
 
-# print(kdd_data_10percent['label'].value_counts())
-num_features = [
-    "duration","src_bytes",
-    "dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins",
-    "logged_in","num_compromised","root_shell","su_attempted","num_root",
-    "num_file_creations","num_shells","num_access_files","num_outbound_cmds",
-    "is_host_login","is_guest_login","count","srv_count","serror_rate",
-    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
-    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
-    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
-    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
-    "dst_host_rerror_rate","dst_host_srv_rerror_rate"
-]
 features = kdd_data_10percent[num_features].astype(float)
 print(features.describe())
 
-from sklearn.neighbors import KNeighborsClassifier
 labels = kdd_data_10percent['label'].copy()
 labels[labels!='normal.'] = 'attack.'
-# print(labels.value_counts())
 
 from sklearn.preprocessing import MinMaxScaler
 minmax = MinMaxScaler()
 features = pd.DataFrame(minmax.fit_transform(features.values), columns=num_features)
-# print(features)
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 clf = KNeighborsClassifier(n_neighbors = 5, algorithm = 'ball_tree', leaf_size=500)
+# clf = GaussianNB()
 t0 = time()
 clf.fit(features,labels)
 tt = time()-t0
@@ -53,7 +28,8 @@ kdd_data_corrected = pd.read_csv("corrected.txt", header=None, names=names)
 print(kdd_data_corrected['label'].value_counts())
 print(kdd_data_corrected.describe())
 
-kdd_data_corrected['label'][kdd_data_corrected['label']!='normal.'] = 'attack.'
+# kdd_data_corrected['label'][kdd_data_corrected['label']!='normal.'] = 'attack.'
+kdd_data_corrected.loc[(kdd_data_corrected['label'] != 'normal.'), 'label'] = 'attack.'
 print(kdd_data_corrected['label'].value_counts())
 
 from sklearn.model_selection import train_test_split
@@ -69,39 +45,14 @@ features_train, features_test, labels_train, labels_test = train_test_split(
     random_state=42
     )
 
-t0 = time()
 pred = clf.predict(features_test)
-tt = time() - t0
-print("Predicted in {} seconds".format(round(tt,3)))
+print(pred)
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,classification_report, confusion_matrix
 acc = accuracy_score(pred, labels_test)
+cn_mt = confusion_matrix(pred.astype(str), labels_test.astype(str))
+cl_rp = classification_report(pred, labels_test)
+print(cn_mt)
+print(cl_rp)
 print(acc)
-print("R squared is {}.".format(round(acc,4)))
 
-from sklearn.cluster import KMeans
-k = 30
-km = KMeans(n_clusters = k)
-
-
-t0 = time()
-km.fit(features)
-tt = time()-t0
-print("Clustered in {} seconds".format(round(tt,3)))
-
-print(pd.Series(km.labels_).value_counts())
-
-labels = kdd_data_10percent['label']
-label_names = list(map(
-    lambda x: pd.Series([labels[i] for i in range(len(km.labels_)) if km.labels_[i]==x]), 
-    range(k)))
-
-for i in range(k):
-    print("Cluster {} labels:".format(i))
-    print(label_names[i].value_counts())
-    print()
-
-t0 = time()
-pred = km.predict(kdd_data_corrected[num_features])
-tt = time() - t0
-print("Assigned clusters in {} seconds".format(round(tt,3)))
--- knn_ids.py
+++ knn_ids.py
@@ -1,66 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from utils.codes import names
 from sklearn import preprocessing
 
 # Change the following variables as desired.
 # 'k_value' is any positive integer
 k_value = 5
 
-# Assign column names to the dataset
-names = ['duration', 'protocol_type', 'service',\
-    'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment',\
-    'urgent', 'hot', 'num_failed_logins', 'logged_in', 'num_compromised',\
-    'root_shell', 'su_attempted', 'num_root', 'num_file_creations', 'num_shells',\
-    'num_access_files', 'num_outbound_cmds', 'is_host_login', 'is_guest_login', 'count',\
-    'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',\
-    'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate', 'dst_host_count',\
-    'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',\
-    'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',\
-    'dst_host_srv_serror_rate', 'dst_host_rerror_rate',' dst_host_srv_rerror_rate', 'class']
-
 # Read dataset to pandas dataframe
-train_dataset = pd.read_csv("KDDTrain+20Percent.txt", names=names)
+# train_dataset = pd.read_csv("KDDTrain+20Percent.txt", names=names)
+train_dataset = pd.read_csv("KDD10.txt", names=names)
 
 # Label encoder transforms the dataframe into integers accordinglyp
 label_encoder = preprocessing.LabelEncoder()
-transform_train = train_dataset.iloc[:, :-2].apply(label_encoder.fit_transform)
+transform_train = train_dataset.iloc[:, :-1].apply(label_encoder.fit_transform)
+train_dataset.loc[(train_dataset['label'] != 'normal.'), 'label'] = 'anomaly.'
 
-X_train_fit = transform_train.iloc[:, :-2].values
+X_train_fit = transform_train.values
 y_train_fit = train_dataset.iloc[:, 41].values
 
-test_dataset = pd.read_csv("KDDTest.txt", names=names)
+# test_dataset = pd.read_csv("KDDTest.txt", names=names)
+test_dataset = pd.read_csv("corrected.txt", names=names)
 
-transform_test = test_dataset.iloc[:, :-2].apply(label_encoder.fit_transform)
+transform_test = test_dataset.iloc[:, :-1].apply(label_encoder.fit_transform)
+test_dataset.loc[(test_dataset['label'] != 'normal.'), 'label'] = 'anomaly.'
 
-X_test_fit = transform_test.iloc[:, :-2].values
+X_test_fit = transform_test.values
 y_test_fit = test_dataset.iloc[:, 41].values
 
 # Normalize the data for more accurate results
-scaler = preprocessing.StandardScaler()
-scaler.fit(X_train_fit)
-X_train = scaler.transform(X_train_fit)
-X_test = scaler.transform(X_test_fit)
+minmax = preprocessing.MinMaxScaler()
+minmax.fit(X_train_fit)
+X_train = minmax.transform(X_train_fit)
+X_test = minmax.transform(X_test_fit)
+
+from sklearn.naive_bayes import GaussianNB
+bayes = GaussianNB()
+bayes.fit(X_train_fit, y_train_fit)
+
+y_pred = bayes.predict(X_test_fit)
+
+# Print out the results
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+print(confusion_matrix(y_test_fit.astype(str), y_pred.astype(str)))
+print(classification_report(y_test_fit, y_pred))
+b_acc = accuracy_score(y_pred, y_test_fit)
+print(f"Bayes Classifier Accuracy: {b_acc}")
 
 # Apply the k-nearest neighbors classifier
 from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier(n_neighbors=k_value)
-classifier.fit(X_train, y_train_fit)
+classifier.fit(X_train_fit, y_train_fit)
 
-y_pred = classifier.predict(X_test)
-print(y_pred)   
+y_pred = classifier.predict(X_test_fit)
 
 # Print out the results
-from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(y_test_fit.astype(str), y_pred.astype(str)))
 print(classification_report(y_test_fit, y_pred))
+k_acc = accuracy_score(y_pred, y_test_fit)
+print(f"K Nearest Neighbor Classifier Accuracy: {k_acc}")
 
 # Calculating error for K values between 1 and 40
 error = []
 for i in range(1, 40):
     knn = KNeighborsClassifier(n_neighbors=i)
-    knn.fit(X_train, y_train_fit)
-    pred_i = knn.predict(X_test)
+    knn.fit(X_train_fit, y_train_fit)
+    pred_i = knn.predict(X_test_fit)
     error.append(np.mean(pred_i != y_test_fit))
     print(error[i-1])
 
